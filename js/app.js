d15d3068b465ce4485b101e0e760198d8506c567
dylan <dylancabral.93@gmail.com>
2022-10-05 19:25:19 -0700

    about me class 3 lab updating css java and html

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 'use strict'
+/* Work in the same repository as you did for Lab 2, and modify your README.md file to describe your project with the new features you are adding.
 
-let booleanValue =true;
+*/
 
-if(booleanValue === true ) {
-  console.log("if condition is true this will print in the console")
-}
 
-let trueValue = true;
-let falseValue = false;
+let score = 0;
 
 let username = prompt('What is your name?');
 document.write('Thanks for coming to my page ' + username );
@@ -20,6 +17,7 @@ let dogs = prompt('Do I prefer dogs better over cats?').toLowerCase();
 if (dogs === 'yes'|| dogs ==='y'|| dogs === 'ya' || dogs === 'yup'){
   // console.log("You are correct, I love dogs!");
   alert("You are correct, I love dogs!");
+  score ++;
  } else if (dogs === 'no' || dogs === 'nope' || dogs === 'nah' || dogs === 'n'){
     //console.log('why on earth would I like cats more than dogs, they are great, but not superior Im sorry.');
     alert('why on earth would I like cats more than dogs, they are great, but not superior Im sorry.');
@@ -35,6 +33,7 @@ if (dogs === 'yes'|| dogs ==='y'|| dogs === 'ya' || dogs === 'yup'){
   } else if (music === 'no' || music === 'nope' || music === 'nah' || music === 'n'){
     //console.log('Good guess, Yeah I actually am a huge fan of heavier music like metalcore or deathcore! bluesier country is alright though.');
     alert('Good guess, Yeah I actually am a huge fan of heavier music like metalcore or deathcore! bluesier country is alright though.');
+    score++;
   } else {
      console.log('yes or no, Come on!');
   }
@@ -43,6 +42,7 @@ if (dogs === 'yes'|| dogs ==='y'|| dogs === 'ya' || dogs === 'yup'){
 if (computer === 'yes'|| computer ==='y'|| computer === 'ya' || computer === 'yup'){
   //console.log('Ive had both! But I absolutley love my pc, probably Im a dweeb and love pc gaming!');
   alert('Ive had both! But I absolutley love my pc, probably Im a dweeb and love pc gaming!');
+  score++;
  } else if (computer === 'no' || computer === 'nope' || computer === 'nah' || computer === 'n'){
   //console.log('WRONG! Macs are cool though too.');
   alert('WRONG! Macs are cool though too.');
@@ -58,9 +58,54 @@ if (siblings === 'yes'|| siblings ==='y'|| siblings === 'ya' || siblings === 'yu
  } else if (siblings === 'no' || siblings === 'nope' || siblings === 'nah' || siblings === 'n'){
   //console.log('Yeah, you got it, oldest of 4.');
   alert('Yeah, you got it, oldest of 4.');
+  score++;
  } else {
     //console.log('I seriously cant with you, please for the love of a non denominational diety, yes or no!');
     alert('I seriously cant with you, please for the love of a non denominational diety, yes or no!');
  }
+
+ let attempts = 4
+ let number = '13'
+ let guess = prompt('At what age did I start listening to heavy music?');
  
- 
+ for (let i= 1; i < attempts; i++){
+   // attempts--; 
+   console.log(attempts);
+   console.log(i);
+   if(guess === number){
+     alert('Thats correct, you got it!');
+     score++;
+   } else if (guess > number){
+     alert(`you have ${attempts - i} attempts remaining`);
+     alert('Sorry, maybe try a little younger');
+     guess = prompt('guess again!');
+     
+   } else if (guess < number){
+     alert('not quite that young, guess again');
+     alert(`you have ${attempts - i} attempts remaining`);
+     guess = prompt('guess again!');
+   } 
+   if (i === attempts - 1){
+     console.log('you ran out of attempts the answer was 13'); 
+   }
+
+  }
+
+
+let myFavSeason = ['summer', 'fall', 'hockey'];
+let attemptsRemaining = 7;
+while (attemptsRemaining) {
+  let userGuess = prompt('What\'s my favorite season of the year?').toLowerCase();
+  attemptsRemaining--;
+  for (let i = 0; i < myFavSeason.length; i++) {
+    if (userGuess === myFavSeason[i]) {
+      alert('Yeah, I actually have 2, summer, fall, and hockey.');
+      score++;
+      attemptsRemaining = 0;
+    }
+  }
+  if (attemptsRemaining) {
+    alert(`You have ${attemptsRemaining} attempts remaining.`);
+  }
+}
+alert(`score  ${score} `);
